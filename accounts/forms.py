290c7b034a08d05ee9c04593c455563e3b530566
安齋宇宙 <anzaiuchu@anzaisoranoMacBook-Pro.local>
2023-12-14 00:17:38 +0900

    accounts

---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "account_id",
+            "email",
+            "first_name",
+            "last_name",
+            "birth_date",
+        )
+
+class LoginFrom(AuthenticationForm):
+    class Meta:
+        model = User
