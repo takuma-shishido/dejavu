c688fa3b1171fe7d5296d8a2a9d7f10a95063acd
amitB034 <amit.bhan0223@gmail.com>
2023-12-20 11:45:08 +0900

    *要編集!!*続きを書く画面１回目

--- dejavu_app/urls.py
+++ dejavu_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-app_name = 'dejavu_app'
-urlpatterns = [
 
+app_name = 'dejavu_app'
+
+urlpatterns = [
+    path('write_continue/', views.write_continue, name="write_continue"),
 ]
--- dejavu_app/views.py
+++ dejavu_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def write_continue(request):
+    sample_book_info = ["title", "img", "description", "story"]
+
+    context = {"sample_book_info" : sample_book_info}
+    return render(request, "write_continue.html", context)
