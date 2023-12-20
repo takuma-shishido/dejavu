a202b2ba5ac4c670599fa2c8305d07bdec22acdb
NakazatoTaichi <taichi.nakazoto0521@gmail.com>
2023-12-20 19:47:09 +0900

    create_novel画面作成

--- dejavu_app/urls.py
+++ dejavu_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 app_name = 'dejavu_app'
 urlpatterns = [
-
+    path('create/', views.create_novel, name="create_novel"),
 ]
--- dejavu_app/views.py
+++ dejavu_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_novel(request):
+    create_novel_info = ["title", "imag"]
+    context = {"create_novel_info" : create_novel_info}
+    return render(request, "create_novel.html", context)
